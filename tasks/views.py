2c293ab3b12aaedb2ae7c67892ebeb7acdb4954c
hamid karimi <139635310+Hamidkarimi-h@users.noreply.github.com>
2026-06-17 22:36:41 +0330

    Add create task endpoint

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
-# Create your views here.
+
+@csrf_exempt
+@login_required
+def create_task(request):
+
+    if request.method != "POST":
+        return JsonResponse(
+            {"error": "Method not allowed"},
+            status=405
+        )
+
+    data = json.loads(request.body)
+
+    task = Task.objects.create(
+        user=request.user,
+        title=data.get("title"),
+        description=data.get("description")
+    )
+
+    return JsonResponse(
+        {
+            "id": task.id,
+            "message": "Task created successfully"
+        },
+        status=201
+    )
